08fd75b09e1614552bdf3c832824a25e4e8bbb08
glorygalaxy726-star <glorygalaxy726@gmail.com>
2026-04-15 22:34:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -117,21 +117,21 @@ elif page == "Mineral Scanner":
 
             # Step 2: Math & Calculations
             # --- 1. THE CALCULATION LOOP ---
-        for k, v in extracted.items():
-           m = CHEMICAL_MAP[k]
-           e_pct = v * m['factor']
+               for k, v in extracted.items():
+                   m = CHEMICAL_MAP[k]
+                   e_pct = v * m['factor']
     
-    # Define it here...
-          price_val = e_pct * m['price'] 
+                # Define it here...
+                  price_val = e_pct * m['price'] 
     
-         val_data.append({
-           "Mineral": m['label'],
-           "Oxide %": round(v, 2),
-           "Element %": round(e_pct, 4),
-        # ...and use the SAME name here!
-           "Value (TZS/MT)": round(price_val, 2) 
-           })
-       total_value += price_val # Use it here too
+                  val_data.append({
+                 "Mineral": m['label'],
+                 "Oxide %": round(v, 2),
+                 "Element %": round(e_pct, 4),
+               # ...and use the SAME name here!
+                 "Value (TZS/MT)": round(price_val, 2) 
+                })
+                total_value += price_val # Use it here too
 
                 # Step 3: Display Results
               # 1. Convert your list of results into a DataFrame

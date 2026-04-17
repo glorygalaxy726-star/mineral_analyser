069d5cf867380b1663287c5bdf4b6dd9088b041a
glorygalaxy726-star <glorygalaxy726@gmail.com>
2026-04-17 21:23:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -159,15 +159,15 @@ else:
      st.caption("Developed by Glory Benson | Chemist & Digital Researcher | 0616648724")
 # --- STEP 2: AUTHORIZED ACCESS ---
 # This part only runs if st.session_state.logged_in is True
-     else:
+    else:
          # 💎 Now the Sidebar Menu appears!
-         st.sidebar.title(f"💎 Welcome, {st.session_state.current_user}")
-         nav_selection = st.sidebar.radio("Go to:", ["Welcome Home", "Thamani Mineral Analytics"])
+        st.sidebar.title(f"💎 Welcome, {st.session_state.current_user}")
+        nav_selection = st.sidebar.radio("Go to:", ["Welcome Home", "Thamani Mineral Analytics"])
     
-         if st.sidebar.button("Logout"):
-            st.session_state.logged_in = False
-            st.rerun()
-            st.caption("Developed by Glory Benson | Chemist & Digital Researcher | 0616648724")
+        if st.sidebar.button("Logout"):
+           st.session_state.logged_in = False
+           st.rerun()
+           st.caption("Developed by Glory Benson | Chemist & Digital Researcher | 0616648724")
 
 # --- PAGE: WELCOME HOME ---
      if nav_selection == "Welcome Home":

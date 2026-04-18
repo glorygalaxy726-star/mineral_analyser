7ccaee6d98d24037915ddecf33a28065a8d511d4
glorygalaxy726-star <glorygalaxy726@gmail.com>
2026-04-18 17:53:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -240,8 +240,8 @@ else:
                                     # --- DATA EXTRACTION ---
                     # --- SMART EXTRACTION ---
                     # This looks for all common lab names for Moisture and LOI
-                    moisture_val = extracted.get("H2O",0) or extracted.get("MOISTURE",0) or extracted.get("Moisture",0) or extracted.get("H2O (%)",0)
-                    loi_val = extracted.get("LOI",0) or extracted.get("Loss on Ignition",0) or extracted.get("LOI (%)",0)
+                    moisture_val = extracted.get("H2O") or extracted.get("MOISTURE") or extracted.get("Moisture") or extracted.get("H2O (%)")
+                    loi_val = extracted.get("LOI") or extracted.get("Loss on Ignition") or extracted.get("LOI (%)")
                     try:
                         if moisture_val is not None:
                             moisture_val = float(str(moisture_val).replace('%', '').strip())
